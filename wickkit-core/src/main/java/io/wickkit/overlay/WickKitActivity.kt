0db56a0d72e94e2247871e9202c9ec34baf062a6
Jorge Sanzo Hernando <jorgesanzo21@gmail.com>
2026-09-17 13:45:21 +0200

    fix(WKK-44): Fix visual issues

--- wickkit-core/src/main/java/io/wickkit/overlay/WickKitActivity.kt
+++ wickkit-core/src/main/java/io/wickkit/overlay/WickKitActivity.kt
@@ -1,6 +1,6 @@
 package io.wickkit.overlay
 
-import android.app.Activity
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,10 +11,17 @@ import io.wickkit.overlay.ui.WickKitTheme
 
 internal class WickKitActivity : ComponentActivity() {
 
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         disableSystemTransitions()
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isStatusBarContrastEnforced = false
+            window.isNavigationBarContrastEnforced = false
+        }
         setContent {
             WickKitTheme {
                 WickKitScreen(onClose = ::finish)
@@ -25,17 +32,15 @@ internal class WickKitActivity : ComponentActivity() {
     override fun finish() {
         super.finish()
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            @Suppress("DEPRECATION")
             overridePendingTransition(0, 0)
         }
     }
 
     private fun disableSystemTransitions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            overrideActivityTransition(Activity.OVERRIDE_TRANSITION_OPEN, 0, 0)
-            overrideActivityTransition(Activity.OVERRIDE_TRANSITION_CLOSE, 0, 0)
+            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, 0, 0)
+            overrideActivityTransition(OVERRIDE_TRANSITION_CLOSE, 0, 0)
         } else {
-            @Suppress("DEPRECATION")
             overridePendingTransition(0, 0)
         }
     }
